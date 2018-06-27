41fee480f83d94b2ca735116ac09f942ded45ada
Joey Breslin <c15713979@mydit.ie>
2018-06-27 18:27:24 +0100

    KOR Component System, Scriptable Object

---
+++ Kor/Assets/Scripts/Component System/KorComponent.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Component", menuName = "KOR Component")]
+public class KorComponent : ScriptableObject
+{
+    public enum KorComponentType { HEAD, ARMS, LEGS, TORSO };
+
+    [Header("KOR Component Type: ")]
+    public KorComponentType korComponentType;
+
+    [Header("Component Title: ")]
+    public string title;
+
+    [Header("Component Description: ")]
+    [TextArea()]
+    public string description;
+
+    [Header("Component Position: ")]
+    public Vector2 position;
+
+    [Header("Component Sprite: ")]
+    public Sprite sprite;
+
+    [Header("Modifiers")]
+    public float hitPoints,
+                    strength,
+                    defence,
+                    attack,
+                    agility,
+                    stamina;
+
+
+
+
+
+}
